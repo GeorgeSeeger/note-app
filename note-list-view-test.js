38a0d2482b8d7952230d7f0bf49b00ff773cf957
George Seeger <georgeseeger@hotmail.com>
2017-01-03 14:16:10 +0000

    Multiple note test added

--- note-list-view-test.js
+++ note-list-view-test.js
@@ -1,16 +1,10 @@
-
-
 function testingEmptyList(){
   var list = new NoteListHTML();
 
   if(list.view() !== "<ul></ul>"){
     throw new Error("This is not an empty html list")
   }
-
 };
-
-testingEmptyList()
-
 
 function testingNoteAddedInNoteList() {
   var list = new NoteListHTML();
@@ -19,7 +13,20 @@ function testingNoteAddedInNoteList() {
   if(list.view() !== "<ul><li>I like Makers</li></ul>"){
     throw new Error("list does not contain I like Makers ")
   }
-
 };
 
-testingNoteAddedInNoteList()
+function testingMultipleAddedNotes(){
+  var list = new NoteListHTML();
+  list.addNote({ text: "I like Makers" });
+  list.addNote({ text: "I like Apples" });
+  list.addNote({ text: "I like Croissants" });
+  var html = "<ul><li>I like Makers</li><li>I like Apples</li><li>I like Croissants</li></ul>";
+
+  if(list.view() !== html){
+    throw new Error("Multiple notes not displaying");
+  }
+};
+
+testingEmptyList();
+testingNoteAddedInNoteList();
+testingMultipleAddedNotes();
