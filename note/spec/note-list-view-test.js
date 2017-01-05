fca4e0bb7d27c7cc771cf0c53380d39d96bb7db8
Russell Yeo <rusyeo@gmail.com>
2017-01-05 12:33:33 +0000

    update contoller tests

--- note/spec/note-list-view-test.js
+++ note/spec/note-list-view-test.js
@@ -7,22 +7,21 @@
   console.log("This returned: " + noteListView.seeNoteList());
 })();
 
-// (function testNewNoteListViewDisplayHTML() {
-//   var note1        = {readNote: function(){return "Yolo Yolo Yolo Yolo Yolo "}};
-//   var note2        = {readNote:function(){return "Yoko Yoko Yoko Yoko Yoko "}};
-//   var noteList     = {seeList: function(){return [note1, note2]}};
-//   var noteListView = new NoteListView(noteList);
-//   var html         = "<ul><li><div>Yolo Yolo Yolo Yolo </div></li><li><div>Yoko Yoko Yoko Yoko </div></li></ul>";
-//   assert.isTrue(noteListView.displayHTML() === html );
-//   console.log("This returned: " + noteListView.displayHTML());
-// })();
+(function testNewNoteListViewDisplayHTML() {
+  var note1        = {readNote: function(){return "Yolo Yolo Yolo Yolo Yolo "}, id: 0};
+  var note2        = {readNote:function(){return "Yoko Yoko Yoko Yoko Yoko "}, id: 1};
+  var noteList     = {seeList: function(){return [note1, note2]}};
+  var noteListView = new NoteListView(noteList);
+  var html         = "<ul><li><div><a href='#notes/0'>Yolo Yolo Yolo Yolo </a></div></li><li><div><a href='#notes/1'>Yoko Yoko Yoko Yoko </a></div></li></ul>";
+  assert.isTrue(noteListView.displayHTML() === html );
+  console.log("This returned the html");
+})();
 
 (function linkNoteToURl(){
   var note = {readNote: function(){return "Yolo"}, id: 0};
   var noteList = {seeList: function(){return [note]}};
   var noteListView = new NoteListView(noteList);
   var html = "<ul><li><div><a href='#notes/0'>Yolo</a></div></li></ul>";
-  console.log(noteListView.displayHTML());
   assert.isTrue(noteListView.displayHTML() === html );
   console.log("URL is linked");
 })();
